7f632f8e202d4d99c3093d27324746dd6f235747
dvgodoy <dvgodoy@gmail.com>
2025-08-25 18:34:37 +0200

    fixing mixed prec

--- helper_functions.py
+++ helper_functions.py
@@ -129,9 +129,13 @@ def generate(model, tokenizer, sentence, max_new_tokens=64, skip_special_tokens=
     input_ids = tokenized_input["input_ids"].to(model.device)
 
     model.eval()
-    generation_output = model.generate(input_ids=input_ids, 
-                                       eos_token_id=tokenizer.eos_token_id,
-                                       max_new_tokens=max_new_tokens)
+    # if it was trained using mixed precision, uses autocast context
+    ctx = torch.autocast(device_type=model.device.type, dtype=model.dtype) \
+          if model.dtype in [torch.float16, torch.bfloat16] else nullcontext()
+    with ctx:
+        generation_output = model.generate(input_ids=input_ids, 
+                                           eos_token_id=tokenizer.eos_token_id,
+                                           max_new_tokens=max_new_tokens)
     
     output = tokenizer.batch_decode(generation_output, 
                                     skip_special_tokens=skip_special_tokens)[0]
